376bc2c45ac9fb8f3f7e7cf91d813b95b1230b24
вадим круглов <element2266@gmail.com>
2021-10-10 10:21:24 +0300

    101021

--- diplom/ai_bot.py
+++ diplom/ai_bot.py
@@ -1,39 +1,33 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types, filters
 from decouple import config
-from command import command, callback, startpars
+from command import command, callback, startpars, help, cansel
+from register import set_commands
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 bot = Bot(token=config('TOKEN'))
 dp = Dispatcher(bot, storage=MemoryStorage())
+
+#set_commands(dp.bot) # регистрация команд не работает!
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
 
 # START #
 @dp.message_handler(filters.CommandStart())
 async def command_start_handler(msg: types.message):
-    #await msg.answer(f'Привет, чего хотел?')
     await startpars(dp, msg)
 
 # HELP #
 @dp.message_handler(filters.CommandHelp())
-async def command_start_handler(msg: types.message):
-    await msg.answer(f'Привет, чем помочь?')
+async def bot_help(msg: types.message):
+    await help(msg)
 
-# PRIVET #
-@dp.message_handler(filters.Text(startswith=['hi', 'he'], ignore_case=True))
-async def text_contains_all_handler(message: types.message):
-    await message.answer("Хеллоу!")
-
-@dp.message_handler(filters.Text(contains='прив', ignore_case=True))
-@dp.message_handler(filters.Text(contains='здрав', ignore_case=True))
-@dp.message_handler(filters.Text(contains='здоро', ignore_case=True))
-@dp.message_handler(filters.Text(contains='добр', ignore_case=True))
-async def text_contains_all_handler(message: types.message):
-    await message.answer("Привет!")
+# CANSEL #
+@dp.message_handler(state='*', commands=['cancel'])
+async def comand_cansel(msg: types.message, state):
+    await cansel(msg, state)
 
 # Other message #
-# фильтра здесь нет! хандлер ловит все что сюда дошло!
 @dp.message_handler()
 async def user_text(message: types.message):
     await command(dp, message)
--- diplom/command.py
+++ diplom/command.py
@@ -1,13 +1,12 @@
+import asyncio
+from typing import Text
+from states import Form
 from requer import Requer
-from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.dispatcher import FSMContext
-from aiogram.types import InlineKeyboardMarkup as km, InlineKeyboardButton as kb, ReplyKeyboardMarkup as rm, KeyboardButton as btn
+from aiogram.types import InlineKeyboardMarkup as km, InlineKeyboardButton as kb, ReplyKeyboardMarkup as rm, KeyboardButton as btn, ReplyKeyboardRemove as rem
 
 # Словарь ответов
-trust = {'start' : "<i><b>START</b>!</i>", '/hello_world' : "<b>HeLLWorD!</b>"}
-
-class Form(StatesGroup):
-    city = State()
+trust = {'start' : "START!", '/hello_world' : "HeLLWorD!"*3}
 
 # Генерация клавиатуры.
 def get_keyboard():
@@ -22,8 +21,9 @@ def get_keyboard():
 def get_btn():
     return rm(resize_keyboard=True).add(btn('Привет! 👋'), btn('Выбор'))
 
-# START
+# START #
 async def startpars(dp, mes):
+    #await msg.answer(f'Привет, чего хотел?')
     await Form.city.set()
     await mes.answer('введите город')
     @dp.message_handler(state=Form.city)
@@ -34,11 +34,43 @@ async def startpars(dp, mes):
             await msg.reply('не принято')
         await state.finish()
 
+# HELP #
+async def help(mes):
+    #await mes.answer(f'Привет, чем помочь?')
+    text = """
+    <b>
+    Список команд:                 
+    /start       - Начать диалог   
+    /help        - Получить справку
+    /form        - Ввод данных     
+    /cancel      - Сброс           
+    /hello_world - Тест            
+    </b>"""
+    await mes.answer(text, parse_mode="HTML")
+
+# CANSEL #
+async def cansel(mes, state):
+    await mes.answer('Canceled.', reply_markup=rem())
+    await state.finish()
+
+# OTHER COM #
 async def command(dp, mes):
-    if mes.text in trust:
-        await mes.answer(trust[mes.text], parse_mode="HTML")
+    ms, user = mes.text.lower(), mes.from_user.full_name
+    if ms in trust:
+        await edprint(dp, mes, trust[ms])
+    elif any(ms.startswith(w) for w in ['hi', 'hey', 'hello']):
+        await edprint(dp, mes, f"Hello, {user}!")
+    elif any(w in ms for w in ['прив', 'здрав', 'здрас', 'здоров', 'добр']):
+        await edprint(dp, mes, f"Привет, {user}!")
     else:
         await mes.answer("Привет!\nНапиши мне что-нибудь!")
+
+# print message(edit)#10.10.21
+async def edprint(d, mes, ans):
+    mid = await mes.answer('*')
+    for i in range(1, len(ans.split()[0])):
+        await d.bot.edit_message_text(ans[:i], chat_id=mid.chat.id, message_id=mid.message_id)
+    await d.bot.edit_message_text(ans, chat_id=mid.chat.id, message_id=mid.message_id)
 
 
 async def callback(bot, query):
