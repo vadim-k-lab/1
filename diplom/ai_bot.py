887b3b9896192965dbb6911694e54ae77a16d858
вадим круглов <element2266@gmail.com>
2021-10-13 06:04:20 +0300

    dep1

--- diplom/ai_bot.py
+++ diplom/ai_bot.py
@@ -1,9 +1,18 @@
 import logging
-from aiogram import Bot, Dispatcher, executor, types, filters
-from decouple import config
-from command import command, callback, startpars, help, cansel
-from register import set_commands
+import os
+from aiogram import Bot, Dispatcher, executor, filters, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from decouple import config
+from aiogram.utils.executor import start_webhook
+from command import callback, cansel, command, help, startpars
+from register import set_commands
+
+WEBHOOK_HOST = 'https://depbot1.herokuapp.com'  # name your app
+WEBHOOK_PATH = '/webhook/'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.environ.get('PORT')
+#TOKEN = os.environ['TOKEN']
 
 bot = Bot(token=config('TOKEN'))
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -38,4 +47,18 @@ async def process_callback(query: types.callback_query):
     await callback(bot, query)
 
 
-executor.start_polling(dp, skip_updates=True)
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+async def on_shutdown(dp):
+    # insert code here to run it before shutdown
+    pass
+
+if __name__ == '__main__':
+    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
+                  on_startup=on_startup, on_shutdown=on_shutdown,
+                  host=WEBAPP_HOST, port=WEBAPP_PORT)
+
+
+
+#executor.start_polling(dp, skip_updates=True)
